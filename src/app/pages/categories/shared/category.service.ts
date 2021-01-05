67d25dbca28be564ba610b54333440d7e8bb69c4
Silvio Aparecido Capelo <silvio.silva51@gmail.com>
2021-01-05 08:59:17 -0300

    Intercepting http request with angular-in-memory-web-api

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -45,12 +45,14 @@ export class CategoryService {
       map(this.jsonDataToCategory)
     );
   }
+
   create(category: Category): Observable<Category> {
     return this.http.post(this.apiPath, category).pipe(
       catchError( this.handleError),
       map(this.jsonDataToCategory)
     );
   }
+
   update(category: Category): Observable<Category> {
     const url = `${ this.apiPath}/${category.id}`;
     return this.http.put(this.apiPath, category).pipe(
